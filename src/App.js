ad0c6c31a3745581e90cd952cc7e4a854f78dccf
JaeIn <sm810403@hotmail.com>
2020-09-03 21:31:15 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  }
+  getMovies = async ()=>{
+    const data = await axios.get('')
+
+  
+  componentDidMount(){
+    this.getMovies();
+
+    // setTimeout(()=>{
+    //   this.setState({isLoading: false})
+    // }, 6000)
+  }
+  render(){
+    const {isLoading} = this.state;
+    return(
+      <div>{isLoading? 'loading...': 'we are ready'}</div>
+    )
+  }
 }
 
 export default App;
